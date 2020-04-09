fad3b01c7dcda33b08f7a6bff9fe5d762835d13c
Allan Cleysson de Oliveira Ramos <a.de.oliveira.ramos@avanade.com>
2020-04-09 14:56:52 -0300

    Test Objects from homepage

---
+++ Object Repository/Home/button_makeAppointment.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>button_makeAppointment</name>
+   <tag></tag>
+   <elementGuidId>d2d13745-a02f-45ec-93ac-cdb3cf49cc5b</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//a [@id=&quot;btn-make-appointment&quot;]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/Home/button_menuToggle.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>button_menuToggle</name>
+   <tag></tag>
+   <elementGuidId>ad84c681-a9c4-4f65-808c-337361b73a64</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//a [@id=&quot;menu-toggle&quot;]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
