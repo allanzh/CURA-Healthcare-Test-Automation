eea03e1fa45d2dfe16c6c517c05eae5a1ee23707
Allan Cleysson de Oliveira Ramos <a.de.oliveira.ramos@avanade.com>
2020-04-09 15:58:20 -0300

    Test Objects related to Login page

---
+++ Object Repository/Login/input_username.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>input_username</name>
+   <tag></tag>
+   <elementGuidId>3c2605c0-fb2b-44fb-94d9-36d8590fe1cf</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//input [@id='txt-username']</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
